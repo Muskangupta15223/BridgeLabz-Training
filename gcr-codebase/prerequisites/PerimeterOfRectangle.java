6a3e86e43539279d2ed6f698f6ab045a6f01e5d6
Muskan-gupta <144871265+Muskangupta15223@users.noreply.github.com>
2025-12-16 22:42:33 +0530

    [ Muskan Gupta ] : commited PerimeterOfRectangle java program

---
+++ gcr-codebase/prerequisites/PerimeterOfRectangle.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class PerimeterOfRectangle{
+
+  public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    int length = sc.nextInt();
+    int width = sc.nextInt();
+    System.out.println(2*(length+width));
+
+
+}
+
+}
