d1e23dfdb14fe1ba75f717d3cac323eb58d28e71
Muskan-gupta <144871265+Muskangupta15223@users.noreply.github.com>
2025-12-16 20:53:37 +0530

    [ Muskan Gupta ] : commited AreaOfCIrcle java program

---
+++ gcr-codebase/prerequisites/AreaOfCircle.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class AreaOfCircle{
+
+  public static void main(String args[]){
+    Scanner sc = new Scanner(System.in);
+      double PI = 3.14;
+    System.out.println("Give Radius");
+      int radius = sc.nextInt();
+    System.out.println("Area of Circle is: " + PI*radius*radius);
+}}
