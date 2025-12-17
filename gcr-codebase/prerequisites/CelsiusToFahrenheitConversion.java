0984c5f6b0a70677db5965f4e75b19829cf629e0
Muskan gupta <muskangupta15223@gmail.com>
2025-12-17 21:16:38 +0530

    [Muskan Gupta ] : commited CelsiusToFahrenheitConversion java program

--- gcr-codebase/prerequisites/CelsiusToFahrenheitConversion.java
+++ gcr-codebase/prerequisites/CelsiusToFahrenheitConversion.java
@@ -2,9 +2,9 @@ import java.util.*;
 class  CelsiusToFahrenheitConversion{
 public static void main(String args[]){
    Scanner sc = new Scanner(System.in);
-   int cel_val = sc.nextInt();
-   int fah_val;
-   fah_val = cel_val * (9/5)+32;
-   System.out.println(fah_val); 
+   int celVal = sc.nextInt();
+   int fahVal;
+   fahVal = celVal * (9/5)+32;
+   System.out.println(fahVal); 
    }
 }
