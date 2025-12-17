e4474ca791c9f5766a16f47e8e76655870876cb0
Muskan-gupta <144871265+Muskangupta15223@users.noreply.github.com>
2025-12-17 13:42:00 +0530

    [Muskan Gupta] : commited VolumeOfCylinder java program

--- gcr-codebase/prerequisites/VolumeOfCylinder.java
+++ gcr-codebase/prerequisites/VolumeOfCylinder.java
@@ -1,8 +1,7 @@
 import java.util.*;
 class VolumeOfCylinder{
-
-  public static void main(String args[]){
-    Scanner sc = new Scanner(System.in);
+   public static void main(String args[]){
+      Scanner sc = new Scanner(System.in);
       double PI = 3.14;
       System.out.println("Give radius: ");
       int radius = sc.nextInt();
