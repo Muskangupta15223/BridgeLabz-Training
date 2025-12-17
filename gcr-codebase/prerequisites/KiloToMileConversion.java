eb794516e550f6f7272284b9486b17db0d463967
Muskan-gupta <144871265+Muskangupta15223@users.noreply.github.com>
2025-12-17 13:37:41 +0530

    [Muskan Gupta] : commited KiloToMileConversion java program

--- gcr-codebase/prerequisites/KiloToMileConversion.java
+++ gcr-codebase/prerequisites/KiloToMileConversion.java
@@ -1,11 +1,10 @@
 import java.util.*;
 class KiloToMileConversion{
-
    public static void main(String args[]){
      Scanner sc = new Scanner(System.in);
-   System.out.println("give kilometer value");
-    double kilometers = sc.nextInt();
-    double miles = kilometers * 0.621371; 
-    System.out.println(miles);
+     System.out.println("give kilometer value");
+     double kilometers = sc.nextInt();
+     double miles = kilometers * 0.621371; 
+     System.out.println(miles);
    }
 }
