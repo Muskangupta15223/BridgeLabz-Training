610f08de0a63efafa931967316277019bba0fad8
Muskan-gupta <144871265+Muskangupta15223@users.noreply.github.com>
2025-12-17 14:26:25 +0530

    [Muskan Gupta] : commited AverageOfThreeNumbers java program

--- gcr-codebase/prerequisites/AvgOfThreeNumber.java
+++ gcr-codebase/prerequisites/AvgOfThreeNumber.java
@@ -5,7 +5,8 @@ class AvgOfThreeNum{
      int num1 = sc.nextInt();
      int num2 = sc.nextInt();
      int num3 = sc.nextInt();
-     double avg = (num1 + num2 + num3)/100;
+     double avg = (num1 + num2 + num3)/3;
      System.out.println("Average Of Three Numbers: " + avg);
   }
 }
+
