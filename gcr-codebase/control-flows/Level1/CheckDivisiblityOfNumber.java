efd59d523be909a00943184478fde0e6d7d34d9f
Muskan gupta <muskangupta15223@gmail.com>
2025-12-18 15:28:17 +0530

    [Muskan Gupta ] : commited  CheckDivisiblityOfNumber java program

--- gcr-codebase/control-flows/Level1/CheckDivisiblityOfNumber.java
+++ gcr-codebase/control-flows/Level1/CheckDivisiblityOfNumber.java
@@ -2,8 +2,11 @@ import java.util.*;
 //Creating class CheckDivisibleBy5 to check number is divisible by 5 or not
 public class  CheckDivisiblityOfNumber {
 	public static void main(String args[]){
+		//Taking number as input from user
 		Scanner sc = new Scanner(System.in);
 		int number = sc.nextInt();
+		
+		//display output
 		System.out.println("Is the number " + number + " divisible by 5? " + (number % 5 == 0));
 	}
 }
