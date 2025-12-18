f025da91bd4abde3a8f4824ef436d27295a1058b
Muskan gupta <muskangupta15223@gmail.com>
2025-12-18 14:23:27 +0530

    [Muskan Gupta ] : commited BasicCalculator java program

---
+++ gcr-codebase/programming-elements/Level2/BasicCalculator.java
@@ -0,0 +1,19 @@
+import java.util.*;
+// Creating class BasicCalculator for doing calculation using arithematic operations
+public class BasicCalculator{
+	public static void main(String args[]){
+		//Taking num1 and num2 as input from user
+		Scanner sc = new Scanner(System.in);
+		float num1 = sc.nextInt();
+		float num2 = sc.nextInt();
+		
+		float addition = num1 + num2 ;
+		float subtraction = num1 - num2 ;
+		float multiplication = num1 * num2;
+		float division = num1/ num2;
+		
+		System.out.println("The addition, subtraction, multiplication, and division value of 2 numbers " + num1 + " and " + num2 + " is "+ addition + " , " + subtraction + ", " + multiplication + ", and " + division);
+
+	}
+}
+
