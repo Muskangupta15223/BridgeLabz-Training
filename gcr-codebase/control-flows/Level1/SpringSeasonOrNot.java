8ddcdf78d117a4e2624455d23405076416b081eb
Muskan gupta <muskangupta15223@gmail.com>
2025-12-19 15:41:37 +0530

    [ Muskan Gupta ] : commited  SpringSeasonOrNot java program

---
+++ gcr-codebase/control-flows/Level1/SpringSeasonOrNot.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class  SpringSeasonOrNot{
+	public static void main(String args[]){
+		// taking date and month as input from user using command line
+		int month = Integer.parseInt(args[0]);
+ 		int day = Integer.parseInt(args[1]);
+
+                 if ((month == 3 && date >= 20 && date <= 31 ) || (month == 4 && date >= 1 && date <= 30) ||
+                     (month == 5 && date >= 1 && date <= 31) || (month == 6 && date >= 1 && date <= 20)){
+			System.out.println(" Spring Season ");
+		}else{
+			System.out.println("Not a Spring Season");
+
+		}
+	}
+}
