865745eb408c744b7801f61cce2916dd4c6950c0
Muskan gupta <muskangupta15223@gmail.com>
2025-12-23 23:11:10 +0530

    [Muskan Gupta ] : commited FindSubString java program

---
+++ gcr-codebase/string/Level1/FindSubString.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class FindSubString{
+
+	public static char[] convertToCharArray(String s){
+		int size = s.length();
+		char[] arr = new char[size];
+		for(int i = 0; i < size; i++){
+			arr[i] = s.charAt(i);
+		}
+		return arr;
+	}
+
+	public static boolean comparison(char[] s, String str){
+		char[] t = str.toCharArray();
+		return Arrays.equals(s,t);
+		  
+	}
+	public static void main(String args[]){
+		Scanner sc = new Scanner(System.in);
+		String str = sc.next();
+		
+		//display output
+		char[] ch = convertToCharArray(str);
+		System.out.println("Compare character array by using built in & without using built in :  \n" + comparison(ch,str));
+
+	}
+}
