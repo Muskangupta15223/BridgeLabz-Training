840b27132b9b77f7088bcbaada69035373abb124
Muskan gupta <muskangupta15223@gmail.com>
2025-12-28 00:50:13 +0530

    [ Muskan Gupta] : commited Product java program

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcept/src/com/constructors/levelone/ProductDetails/MainProduct.java
@@ -0,0 +1,20 @@
+package com.constructors.levelone.ProductDetails;
+
+public class MainProduct {
+
+	public static void main(String[] args) {
+		//create obj and call class and instance methods 
+		  Product p1 = new Product("Laptop", 55000);
+	        Product p2 = new Product("Headphone", 1200);
+	        Product p3 = new Product("Printer", 25000);
+	        
+	        p1.displayProductDetails();
+	        System.out.println();
+	        p2.displayProductDetails();
+	        System.out.println();
+	        p3.displayProductDetails();
+	        System.out.println();
+	        
+	        Product.displayTotalProducts();
+	}
+}
