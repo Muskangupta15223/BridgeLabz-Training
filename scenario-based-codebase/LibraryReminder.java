bb512a26e6ac14bee22f29ded176aa1bda0031b5
Muskan gupta <muskangupta15223@gmail.com>
2025-12-30 16:43:17 +0530

    [Muskan Gupta ] : commited LibraryReminder java program

---
+++ scenario-based-codebase/LibraryReminder.java
@@ -0,0 +1,32 @@
+import java.time.LocalDate;
+import java.util.*;
+import java.time.temporal.ChronoUnit;
+
+public class LibraryReminder {
+
+    public static int fineCalculator(LocalDate dueDate, LocalDate returnDate) {
+        long daysLate = ChronoUnit.DAYS.between(dueDate, returnDate);
+        int finePay = (int) daysLate * 10;
+        return finePay;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Welcome to Library Reminder System");
+        System.out.println("Enter the date when the book was taken (YYYY-MM-DD): ");
+        LocalDate takenDate = LocalDate.parse(sc.next());
+        LocalDate dueDate = takenDate.plusDays(14);
+        System.out.println("The return date of the book(YYYY-MM-DD):");
+        LocalDate returnDate = LocalDate.parse(sc.next());
+
+        if (returnDate.isAfter(dueDate)) {
+            System.out.println("You have returned the book late.");
+            int fine = fineCalculator(dueDate, returnDate);
+            System.out.println("Fine to be paid: Rs " + fine);
+        } else {
+            System.out.println("You have returned the book on time. No fine.");
+        }
+
+    }
+
+}
