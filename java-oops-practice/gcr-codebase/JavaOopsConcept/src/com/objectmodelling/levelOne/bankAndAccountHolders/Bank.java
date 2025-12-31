8fb6b4b72af05c782d9f4bfd8362d735bcadb171
Muskan gupta <muskangupta15223@gmail.com>
2025-12-31 13:23:21 +0530

    [Muskan Gupta ] : commited MovieTicketBooking java program

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcept/src/com/objectmodelling/levelOne/bankAndAccountHolders/Bank.java
@@ -0,0 +1,37 @@
+package com.objectmodelling.levelOne.bankAndAccountHolders;
+
+import java.util.List;
+import java.util.ArrayList;
+
+class Bank {
+    private String bankName;
+    private List<Customer> customers; // Bank has multiple customers
+
+    public Bank(String bankName) {
+        this.bankName = bankName;
+        this.customers = new ArrayList<>();
+    }
+
+    // Open a new account (add customer)
+    public void openAccount(String customerName) {
+        Customer customer = new Customer(customerName, this);
+        customers.add(customer);
+        System.out.println("Account opened for " + customerName + " in " + bankName);
+    }
+
+    // List all customers
+    public void listCustomers() {
+        System.out.println("Customers of " + bankName + ":");
+        for (Customer c : customers) {
+            System.out.println("- " + c.getName());
+        }
+    }
+
+    public String getBankName() {
+        return bankName;
+    }
+
+    public List<Customer> getCustomers() {
+        return customers;
+    }
+}
---
+++ java-oops-practice/gcr-codebase/JavaOopsConcept/src/com/objectmodelling/levelOne/bankAndAccountHolders/Customer.java
@@ -0,0 +1,46 @@
+package com.objectmodelling.levelOne.bankAndAccountHolders;
+
+class Customer {
+ private String name;
+ private double balance;
+ private Bank bank; // Association: Customer is linked to a Bank
+
+ public Customer(String name, Bank bank) {
+     this.name = name;
+     this.bank = bank;
+     this.balance = 0.0; // initial balance
+ }
+
+ // Deposit money
+ public void deposit(double amount) {
+     if (amount > 0) {
+         balance += amount;
+         System.out.println(name + " deposited $" + amount + ". New balance: $" + balance);
+     } else {
+         System.out.println("Invalid deposit amount.");
+     }
+ }
+
+ // Withdraw money
+ public void withdraw(double amount) {
+     if (amount <= balance && amount > 0) {
+         balance -= amount;
+         System.out.println(name + " withdrew $" + amount + ". Remaining balance: $" + balance);
+     } else {
+         System.out.println("Insufficient balance or invalid amount.");
+     }
+ }
+
+ // View balance
+ public void viewBalance() {
+     System.out.println(name + "'s balance in " + bank.getBankName() + ": $" + balance);
+ }
+
+ public String getName() {
+     return name;
+ }
+
+ public Bank getBank() {
+     return bank;
+ }
+}
