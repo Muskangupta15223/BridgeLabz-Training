9ed774e4cedfada069b8564bb00cb31efb362e5a
Muskan gupta <muskangupta15223@gmail.com>
2026-01-01 12:31:10 +0530

    [Muskan Gupta] : commited VehicleManagementSystem program

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcept/src/com/inheritance/HybridInheritance/VehicleManagementSystem/PetrolVehicle.java
@@ -0,0 +1,19 @@
+package com.inheritance.HybridInheritance.VehicleManagementSystem;
+
+// Create a subclass PetrolVehicle that extends Vehicle and that implements the
+
+public class PetrolVehicle extends Vehicle implements Refuelable{
+    private double fuelTankCapacity;
+
+    // constructor
+    public PetrolVehicle(String model, double maxSpeed, double fuelTankCapacity) {
+        super(model, maxSpeed);
+        this.fuelTankCapacity = fuelTankCapacity;
+    }
+
+    // Override the refuel method from the interface
+    @Override
+    public void refuel() {
+        System.out.println("Refueling vehicle with " + fuelTankCapacity + " liter tank");
+    }
+}
