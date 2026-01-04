bc848ac9f128789f69f813ef20728423df11f834
Muskan gupta <muskangupta15223@gmail.com>
2026-01-05 01:08:45 +0530

    [ Muskan Gupta] : commited inventorymanagementsystem program

---
+++ java-dsa-practice/gcr-codebase/JavaDsaPractice/src/com/linkedlist/singly/inventorymanagementsystem/ItemNode.java
@@ -0,0 +1,16 @@
+package com.linkedlist.singly.inventorymanagementsystem;
+public class ItemNode {
+    int itemId;
+    String itemName;
+    int quantity;
+    double price;
+    ItemNode next;
+
+    ItemNode(int itemId, String itemName, int quantity, double price) {
+        this.itemId = itemId;
+        this.itemName = itemName;
+        this.quantity = quantity;
+        this.price = price;
+        this.next = null;
+    }
+}
