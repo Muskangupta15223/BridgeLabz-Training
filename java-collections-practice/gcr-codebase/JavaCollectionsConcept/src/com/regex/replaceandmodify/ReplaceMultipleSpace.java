9e59421e9421ead9ee739b27136e3e08458fedc1
Muskan gupta <muskangupta15223@gmail.com>
2026-01-27 22:45:52 +0530

    [ Muskan Gupta] : commited regex  ReplaceMultipleSpace java program

---
+++ java-collections-practice/gcr-codebase/JavaCollectionsConcept/src/com/regex/replaceandmodify/ReplaceMultipleSpace.java
@@ -0,0 +1,9 @@
+package com.regex.replaceandmodify;
+
+public class ReplaceMultipleSpace {
+	 public static void main(String[] args) {
+	        String input = "This    is         an example   with   multiple   spaces.";
+	        String output = input.replaceAll("\\s+", " ");
+	        System.out.println(output);
+	    }
+}
