3bd95d912b9ae3086d78d959554da265501b12c7
Muskan gupta <muskangupta15223@gmail.com>
2026-01-27 23:14:25 +0530

    [ Muskan Gupta] : commited ExtractProgrammingLanguage java program

---
+++ java-collections-practice/gcr-codebase/JavaCollectionsConcept/src/com/regex/advance/ExtractProgrammingLanguage.java
@@ -0,0 +1,32 @@
+package com.regex.advance;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.*;
+public class ExtractProgrammingLanguage {
+
+	  public static List<String> extractLanguages(String text) {
+
+	      List<String> languages = List.of(
+	    	"Java", "Python", "JavaScript", "Go", "C", "C++", "C#", "Ruby", "Kotlin"
+          );
+
+	      List<String> result = new ArrayList<>();
+
+	      for (String lang : languages) {
+	           String regex = "\\b" + Pattern.quote(lang) + "\\b";
+	           Pattern pattern = Pattern.compile(regex, Pattern.CASE_INSENSITIVE);
+	           Matcher matcher = pattern.matcher(text);
+
+	           if (matcher.find()) {
+	               result.add(lang);
+	           }
+          }
+            return result;
+	     }
+
+	     public static void main(String[] args) {
+	         String text = "I love Java, Python, and JavaScript, but I haven't tried Go yet.";
+
+	       System.out.println(extractLanguages(text));
+	     }
+}
