cdc2226a8eea9bba66725c08e9f710828756c945
Muskan gupta <muskangupta15223@gmail.com>
2026-01-28 22:34:53 +0530

    [ Muskan Gupta] : commited examscanner java program

---
+++ java-collections-practice/scenario-based/JavaCollectionScenarioBased/src/com/collections/examscanner/ExamScannerMain.java
@@ -0,0 +1,28 @@
+package com.collections.examscanner;
+
+import java.util.*;
+
+public class ExamScannerMain {
+
+    public static void main(String[] args) {
+
+        try {
+            List<String> key = Arrays.asList("A", "B", "C", "D", "A");
+            ExamScanner<String> scanner = new ExamScanner<>(key);
+            Map<String, Integer> result = scanner.evaluate("D:\\BridgeLabz-Training\\java-collections-practice\\scenario-based\\JavaCollectionScenarioBased\\src\\com\\resources\\samplefiles\\answers\\answers.csv");
+            PriorityQueue<Map.Entry<String, Integer>> pq = new PriorityQueue<>((a, b) ->b.getValue() - a.getValue());
+            pq.addAll(result.entrySet());
+
+            System.out.println("=== Rank List ===");
+            while (!pq.isEmpty()) {
+                var e = pq.poll();
+                System.out.println(
+                        e.getKey() + " → " + e.getValue());
+            }
+
+        } catch (Exception e) {
+            System.out.println("ExamScanner failed");
+        }
+    }
+}
+
