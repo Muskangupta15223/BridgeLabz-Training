8bb6370f11b0913f268d37ca8355522c58365e1d
Muskan gupta <muskangupta15223@gmail.com>
2026-01-28 22:26:05 +0530

    [ Muskan Gupta] : commited examresultuploader java program

---
+++ java-collections-practice/scenario-based/JavaCollectionScenarioBased/src/com/collections/examresultuploader/InvalidMarksException.java
@@ -0,0 +1,9 @@
+package com.collections.examresultuploader;
+
+public class InvalidMarksException extends Exception {
+	private static final long serialVersionUID = 1L;
+
+	public InvalidMarksException(String message) {
+		super(message);
+	}
+}
