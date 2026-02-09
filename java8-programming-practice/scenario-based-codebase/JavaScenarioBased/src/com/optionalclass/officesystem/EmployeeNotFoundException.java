4b2c8f0f325ef020062308578f2590ec4da9da37
Muskan gupta <muskangupta15223@gmail.com>
2026-02-09 12:24:39 +0530

    [Muskan Gupta] : commited officesystem optional java program.

---
+++ java8-programming-practice/scenario-based-codebase/JavaScenarioBased/src/com/optionalclass/officesystem/EmployeeNotFoundException.java
@@ -0,0 +1,8 @@
+package com.optionalclass.officesystem;
+
+public class EmployeeNotFoundException extends RuntimeException {
+
+    public EmployeeNotFoundException(String message) {
+        super(message);
+    }
+}
