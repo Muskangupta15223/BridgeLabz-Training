738d01ea274195a65bebab5c03107aa84a92d2f3
Muskan-Gupta <144871265+Muskangupta15223@users.noreply.github.com>
2026-02-12 20:29:45 +0530

    Update database connection credentials

--- dbms-jdbc-practice/gcr-codebase/DbmsJdbcConcepts/src/main/java/com/jdbcPractice/BasicConnection.java
+++ dbms-jdbc-practice/gcr-codebase/DbmsJdbcConcepts/src/main/java/com/jdbcPractice/BasicConnection.java
@@ -7,9 +7,8 @@ import java.sql.SQLException;
 public class BasicConnection {
 // Database credentials
 //	private static final String URL = "jdbc:mysql://localhost:3306/ sample_db";
-	private static final String URL = "jdbc:mysql://localhost:3306/ employeemanagement";
-	private static final String USER = "root";
-	private static final String PASSWORD = "muskan@123";
+private static final String URL = "jdbc:mysql://localhost:3306/ employeemanagement";
+private static final String USER = "root";
 
 	public static Connection getConnection() {
 		Connection connection = null;
